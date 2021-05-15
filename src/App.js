c6574a66447989b02f5251a36d017f4f5b9ec59a
hamdicss <hamdi.belhassen@enis.tn>
2021-05-15 15:47:31 +0200

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import myWonderfulImage from "./imageInSrc.jpg"
 import './App.css';
+import './style.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="title">
+            <h1 className="red"> FC Barcelone </h1>
+        </div>
+        <br ></br>
+        <div className="body">
+         <div className="imageSrc">
+          <img className="src"src={myWonderfulImage} alt="imageInSrc" /> 
+          <h1 className="sousims"> FC Barcelona Team </h1>
+         </div>
+         <div className="imagePub">
+            <img className="pub" src="imageInPublic.jpg" alt="imageInPublic" /> 
+           <h1 className="sousimp"> FC Barcelona Stadium </h1>
+         </div> 
+    </div>
+        <div className="video">
+          <video style={{width:"320",height:"240"}} src="BEKO.mp4" type="video/mp4" controls>
+          </video>
+        </div>  
     </div>
   );
 }
